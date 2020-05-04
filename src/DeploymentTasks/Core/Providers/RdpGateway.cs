839c4d907ef6fec091d107976f7c3f9a7505c366
webprofusion-chrisc <christopher.cook@webprofusion.com>
2020-05-04 13:02:20 +0800

    Implement RDP Deployment tasks

---
+++ src/DeploymentTasks/Core/Providers/RdpGateway.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Certify.Config;
+using Certify.Management;
+using Certify.Models;
+using Certify.Models.Config;
+using Certify.Models.Providers;
+using Plugin.DeploymentTasks.Shared;
+
+namespace Certify.Providers.DeploymentTasks
+{
+    public class RdpGateway : IDeploymentTaskProvider
+    {
+        public static DeploymentProviderDefinition Definition { get; }
+        public DeploymentProviderDefinition GetDefinition(DeploymentProviderDefinition currentDefinition = null) => (currentDefinition ?? Definition);
+
+        private const string SCRIPT_NAME = "RDPGatewayServices.ps1";
+
+        static RdpGateway()
+        {
+            Definition = new DeploymentProviderDefinition
+            {
+                Id = "Certify.Providers.DeploymentTasks.RDPGateway",
+                Title = "Deploy to RDP Gateway Service",
+                IsExperimental = true,
+                UsageType = DeploymentProviderUsage.PostRequest,
+                SupportedContexts = DeploymentContextType.LocalAsService | DeploymentContextType.LocalAsUser,
+                Description = "Deploy latest certificate to RDP Gateway Service using Powershell",
+                ProviderParameters = new List<ProviderParameter>
+                {
+                    new ProviderParameter { Key = "restartServices", Name = "Include Service Restart?", Type= OptionType.Boolean, IsCredential = false, Value="false" },
+                }
+            };
+        }
+
+        public async Task<List<ActionResult>> Execute(ILog log, object subject, DeploymentTaskConfig settings, Dictionary<string, string> credentials, bool isPreviewOnly, DeploymentProviderDefinition definition)
+        {
+
+            var validation = await Validate(subject, settings, credentials, definition);
+
+            if (validation.Any())
+            {
+                return validation;
+            }
+
+            var script = Helpers.ReadStringResource(SCRIPT_NAME);
+
+            var certRequest = subject as CertificateRequestResult;
+
+            log?.Information("Executing command via PowerShell");
+
+            var parameters = new Dictionary<string, object>();
+
+            var scriptResult = await PowerShellManager.RunScript(certRequest, parameters: parameters, scriptContent: script, credentials: credentials);
+
+            return new List<ActionResult> { scriptResult };
+
+        }
+
+        public async Task<List<ActionResult>> Validate(object subject, DeploymentTaskConfig settings, Dictionary<string, string> credentials, DeploymentProviderDefinition definition)
+        {
+            var results = new List<ActionResult>();
+
+            return results;
+        }
+    }
+}
---
+++ src/DeploymentTasks/Core/Providers/RdpListener.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Certify.Config;
+using Certify.Management;
+using Certify.Models;
+using Certify.Models.Config;
+using Certify.Models.Providers;
+using Plugin.DeploymentTasks.Shared;
+
+namespace Certify.Providers.DeploymentTasks
+{
+    public class RdpListener : IDeploymentTaskProvider
+    {
+        public static DeploymentProviderDefinition Definition { get; }
+        public DeploymentProviderDefinition GetDefinition(DeploymentProviderDefinition currentDefinition = null) => (currentDefinition ?? Definition);
+
+        private const string SCRIPT_NAME = "RDPListener.ps1";
+
+        static RdpListener()
+        {
+            Definition = new DeploymentProviderDefinition
+            {
+                Id = "Certify.Providers.DeploymentTasks.RDPListener",
+                Title = "Deploy to RDP Listener Service (Terminal Services)",
+                IsExperimental = true,
+                UsageType = DeploymentProviderUsage.PostRequest,
+                SupportedContexts = DeploymentContextType.LocalAsService | DeploymentContextType.LocalAsUser,
+                Description = "Deploy latest certificate to RDP Listener Service using Powershell",
+                ProviderParameters = new System.Collections.Generic.List<ProviderParameter>()
+            };
+        }
+
+        public async Task<List<ActionResult>> Execute(ILog log, object subject, DeploymentTaskConfig settings, Dictionary<string, string> credentials, bool isPreviewOnly, DeploymentProviderDefinition definition)
+        {
+
+            var validation = await Validate(subject, settings, credentials, definition);
+
+            if (validation.Any())
+            {
+                return validation;
+            }
+
+            var script = Helpers.ReadStringResource(SCRIPT_NAME);
+
+            var certRequest = subject as CertificateRequestResult;
+
+            log?.Information("Executing command via PowerShell");
+
+            var parameters = new Dictionary<string, object>();
+
+            var scriptResult = await PowerShellManager.RunScript(certRequest, parameters: parameters, scriptContent: script, credentials: credentials);
+
+            return new List<ActionResult> { scriptResult };
+        }
+
+        public async Task<List<ActionResult>> Validate(object subject, DeploymentTaskConfig settings, Dictionary<string, string> credentials, DeploymentProviderDefinition definition)
+        {
+            var results = new List<ActionResult>();
+
+            return results;
+        }
+    }
+}
