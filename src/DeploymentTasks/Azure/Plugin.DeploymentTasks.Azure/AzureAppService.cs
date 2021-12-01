19674bd9e9c197e6715443ad6af6bff8757114d9
webprofusion-chrisc <christopher.cook@webprofusion.com>
2021-12-01 14:30:59 +0800

    Azure App Service: add cloud service option and match webapp or function

--- src/DeploymentTasks/Azure/Plugin.DeploymentTasks.Azure/AzureAppService.cs
+++ src/DeploymentTasks/Azure/Plugin.DeploymentTasks.Azure/AzureAppService.cs
@@ -36,9 +36,32 @@ namespace Plugin.DeploymentTasks.Azure
                 ProviderParameters = new List<ProviderParameter>
                 {
                     new ProviderParameter{ Key="service_name", Name="App Service Name", IsRequired=true, IsCredential=false,  Description="e.g. my-example-app", Type= OptionType.String },
-                    new ProviderParameter{ Key="subscriptionid",Name="Subscription Id", IsRequired=true , IsPassword=false, IsCredential=false, Type=OptionType.String}
+                    new ProviderParameter{ Key="subscriptionid",Name="Subscription Id", IsRequired=true , IsPassword=false, IsCredential=false, Type=OptionType.String},
+                    new ProviderParameter{ Key="azure_service",Name="Azure Service", IsRequired=true, IsPassword=false, IsCredential=false, Value="global", OptionsList="global=Azure Cloud; china=Azure China; germany=Azure Germany; usgov=Azure US Government" },
                 }
             };
+        }
+
+        private AzureEnvironment MapAzureServiceToAzureEnvironment(string service)
+        {
+            if (string.IsNullOrEmpty(service))
+            {
+                return AzureEnvironment.AzureGlobalCloud;
+            }
+
+            switch (service.Trim())
+            {
+                case "global":
+                    return AzureEnvironment.AzureGlobalCloud;
+                case "usgov":
+                    return AzureEnvironment.AzureUSGovernment;
+                case "china":
+                    return AzureEnvironment.AzureChinaCloud;
+                case "germany":
+                    return AzureEnvironment.AzureGermanCloud;
+                default:
+                    return AzureEnvironment.FromName(service);
+            }
         }
 
         /// <summary>
@@ -70,34 +93,57 @@ namespace Plugin.DeploymentTasks.Azure
                 return results;
             }
 
+            var azureservice = execParams.Settings.Parameters.FirstOrDefault(c => c.Key == "azure_service")?.Value;
             var serviceName = execParams.Settings.Parameters.FirstOrDefault(c => c.Key == "service_name")?.Value;
             var subscriptionId = execParams.Settings.Parameters.FirstOrDefault(c => c.Key == "subscriptionid")?.Value;
             var tenantId = execParams.Settings.Parameters.FirstOrDefault(c => c.Key == "tenantid")?.Value ?? (execParams.Credentials.ContainsKey("tenantid") ? execParams.Credentials["tenantid"] : null); //tenantid from parameter or credential
 
-            var azureEnvironmentName = "AzureGlobalCloud";
-            var knownEnv = AzureEnvironment.KnownEnvironments;
-
             var svcPrincipalLogin = new ServicePrincipalLoginInformation { ClientId = execParams.Credentials["clientid"], ClientSecret = execParams.Credentials["secret"] };
-            var azureCreds = new AzureCredentials(svcPrincipalLogin, tenantId, AzureEnvironment.FromName(azureEnvironmentName));
+
+            var azureEnvironment = MapAzureServiceToAzureEnvironment(azureservice);
+
+            var azureCreds = new AzureCredentials(svcPrincipalLogin, tenantId, azureEnvironment);
 
             var restClient = RestClient
                 .Configure()
-                .WithEnvironment(AzureEnvironment.FromName(azureEnvironmentName))
+                .WithEnvironment(azureEnvironment)
                 .WithCredentials(azureCreds)
                 .Build();
 
             var azure = new AppServiceManager(restClient, subscriptionId, tenantId);
 
+            var appServiceType = "webapp";
+            string appServiceId = null;
+
             var webapps = await azure.WebApps.ListAsync();
-            var functions = await azure.FunctionApps.ListAsync();
 
             // target could be web app/web app slot, function app/ function app slot
             var targetApp = webapps.FirstOrDefault(a => a.Name.ToLower() == serviceName.ToLower());
 
             if (targetApp == null)
             {
-                execParams.Log.Error($"Azure App Service deployment - App not found: {serviceName}");
-                results.Add(new ActionResult("Azure App Service deployment - App not found: {serviceName}", false));
+
+                // check for function app instead
+                var functions = await azure.FunctionApps.ListAsync();
+                var targetFunction = functions.FirstOrDefault(a => a.Name.ToLower() == serviceName.ToLower());
+
+                if (targetFunction != null)
+                {
+                    appServiceId = targetFunction.Id;
+                    appServiceType = "function";
+
+                }
+            }
+            else
+            {
+                appServiceId = targetApp.Id;
+                appServiceType = "webapp";
+            }
+
+            if (appServiceId == null)
+            {
+                execParams.Log.Error($"Azure App Service deployment - App or function not found: {serviceName}");
+                results.Add(new ActionResult($"Azure App Service deployment - App or function not found: {serviceName}", false));
                 return results;
             }
 
@@ -116,44 +162,86 @@ namespace Plugin.DeploymentTasks.Azure
             try
             {
                 // upload cert and apply it to the SNI binding.
-                var app = await azure.WebApps.GetByIdAsync(targetApp.Id);
-
                 var primaryDomain = managedCert.RequestConfig.PrimaryDomain;
 
                 var pfxBytes = System.IO.File.ReadAllBytes(managedCert.CertificatePath);
 
                 // upload cert if not present
-                var existingCerts = await azure.AppServiceCertificates.ListByResourceGroupAsync(app.ResourceGroupName);
 
-                IAppServiceCertificate certificate;
-                if (!existingCerts.Any(c => c.Thumbprint == managedCert.CertificateThumbprintHash))
+                if (appServiceType == "webapp")
                 {
-                    // https://docs.microsoft.com/en-us/azure/app-service/configure-ssl-certificate#private-certificate-requirements
-                    certificate = await azure.AppServiceCertificates
-                                          .Define($"{primaryDomain}_{DateTime.UtcNow.ToString("yyyyMMdd")}")
-                                          .WithRegion(app.Region)
-                                          .WithExistingResourceGroup(app.ResourceGroupName)
-                                          .WithPfxByteArray(pfxBytes)
-                                          .WithPfxPassword(certPwd)
-                                          .CreateAsync();
+                    var app = await azure.WebApps.GetByIdAsync(appServiceId);
+
+                    var existingCerts = await azure.AppServiceCertificates.ListByResourceGroupAsync(app.ResourceGroupName);
+
+                    IAppServiceCertificate certificate;
+                    if (!existingCerts.Any(c => c.Thumbprint == managedCert.CertificateThumbprintHash))
+                    {
+                        // https://docs.microsoft.com/en-us/azure/app-service/configure-ssl-certificate#private-certificate-requirements
+                        certificate = await azure.AppServiceCertificates
+                                              .Define($"{primaryDomain}_{DateTime.UtcNow.ToString("yyyyMMdd")}")
+                                              .WithRegion(app.Region)
+                                              .WithExistingResourceGroup(app.ResourceGroupName)
+                                              .WithPfxByteArray(pfxBytes)
+                                              .WithPfxPassword(certPwd)
+                                              .CreateAsync();
+                    }
+                    else
+                    {
+                        certificate = existingCerts.First(c => c.Thumbprint == managedCert.CertificateThumbprintHash);
+                    }
+                    // apply cert to binding
+
+                    await app.Update()
+                                 .DefineSslBinding()
+                                     .ForHostname(managedCert.RequestConfig.PrimaryDomain)
+                                     .WithExistingCertificate(certificate.Thumbprint)
+                                     .WithSniBasedSsl()
+                                     .Attach()
+                                 .ApplyAsync();
+
+                    execParams.Log.Information($"Deployed certificate to Azure App Service");
+
+                    results.Add(new ActionResult("Certificate Deployed to Azure App Service", true));
                 }
-                else
+                else if (appServiceType == "function")
                 {
-                    certificate = existingCerts.First(c => c.Thumbprint == managedCert.CertificateThumbprintHash);
+                    var app = await azure.FunctionApps.GetByIdAsync(appServiceId);
+
+                    var existingCerts = await azure.AppServiceCertificates.ListByResourceGroupAsync(app.ResourceGroupName);
+
+                    IAppServiceCertificate certificate;
+                    if (!existingCerts.Any(c => c.Thumbprint == managedCert.CertificateThumbprintHash))
+                    {
+                        // https://docs.microsoft.com/en-us/azure/app-service/configure-ssl-certificate#private-certificate-requirements
+                        certificate = await azure.AppServiceCertificates
+                                              .Define($"{primaryDomain}_{DateTime.UtcNow.ToString("yyyyMMdd")}")
+                                              .WithRegion(app.Region)
+                                              .WithExistingResourceGroup(app.ResourceGroupName)
+                                              .WithPfxByteArray(pfxBytes)
+                                              .WithPfxPassword(certPwd)
+                                              .CreateAsync();
+                    }
+                    else
+                    {
+                        certificate = existingCerts.First(c => c.Thumbprint == managedCert.CertificateThumbprintHash);
+                    }
+                    // apply cert to binding
+
+                    await app.Update()
+                                 .DefineSslBinding()
+                                     .ForHostname(managedCert.RequestConfig.PrimaryDomain)
+                                     .WithExistingCertificate(certificate.Thumbprint)
+                                     .WithSniBasedSsl()
+                                     .Attach()
+                                 .ApplyAsync();
+
+                    execParams.Log.Information($"Deployed certificate to Azure App Service");
+
+                    results.Add(new ActionResult("Certificate Deployed to Azure App Service", true));
                 }
-                // apply cert to binding
 
-                await app.Update()
-                             .DefineSslBinding()
-                                 .ForHostname(managedCert.RequestConfig.PrimaryDomain)
-                                 .WithExistingCertificate(certificate.Thumbprint)
-                                 .WithSniBasedSsl()
-                                 .Attach()
-                             .ApplyAsync();
 
-                execParams.Log.Information($"Deployed certificate to Azure App Service");
-
-                results.Add(new ActionResult("Certificate Deployed to Azure App Service", true));
             }
             catch (AuthenticationFailedException exp)
             {
