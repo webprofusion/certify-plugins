68461a24da8dab71783778354c5b707e7f10c2e6
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-02-07 13:02:50 +0800

    Implement Update Port Binding using netsh Task.

---
+++ src/DeploymentTasks/Core/Providers/Assets/UpdatePortBinding.ps1
@@ -0,0 +1,23 @@
+﻿# - sets the associated service port ssl binding to the new cert using netsh
+
+# For more script info see https://docs.certifytheweb.com/docs/script-hooks.html
+
+param($result, $port, $ip, $appid)
+
+$thumb = $result.ManagedItem.CertificateThumbprintHash
+
+## Apply the cert to the port binding using netsh (remove and add)
+
+# get a new guid:
+if ($appid -eq $null)
+{
+	$appid = [guid]::NewGuid()
+}
+
+$ipport ="${ip}:${port}"
+
+# remove the previous certificate:
+& netsh http delete sslcert ipport=$ipport
+
+# set the current certificate:
+& netsh http add sslcert ipport=$ipport certhash=$thumb appid=`{$appid`}
