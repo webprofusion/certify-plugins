facf7b4313e11e6004bff4ecec8769f02dda6794
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-07-21 14:03:39 +0800

    Task: Update port Binding - fix validation, improve netsh error handling

--- src/DeploymentTasks/Core/Providers/Assets/UpdatePortBinding.ps1
+++ src/DeploymentTasks/Core/Providers/Assets/UpdatePortBinding.ps1
@@ -8,16 +8,28 @@ $thumb = $result.ManagedItem.CertificateThumbprintHash
 
 ## Apply the cert to the port binding using netsh (remove and add)
 
-# get a new guid:
+# if the supplied appid is null, use an arbitrary default. It's purpose is to help the user identify the associated app
 if ($appid -eq $null)
 {
-	$appid = [guid]::NewGuid()
+	$appid = "c0a1e5ce-c001-c0de-ba5e-000000000042"
 }
 
-$ipport ="${ip}:${port}"
+$ipport ="${ip}`:${port}"
 
-# remove the previous certificate:
-& netsh http delete sslcert ipport=$ipport
+# remove the previous certificate, redirect errors etc to std out:
+$deleteBindingCmd = "netsh http delete sslcert ipport=${ipport}"
+Write-Output "Deleting Existing Binding: [$deleteBindingCmd]"
+$deleteResult = Invoke-Expression $deleteBindingCmd
 
-# set the current certificate:
-& netsh http add sslcert ipport=$ipport certhash=$thumb appid="{$appid}"
+# set the current certificate, report errors back:
+$addBindingCmd = "netsh http add sslcert ipport=${ipport} certhash=${thumb} appid='{$appid}'"
+Write-Output "Adding Updated Binding: [$addBindingCmd] "
+$addResult = Invoke-Expression $addBindingCmd 
+
+if ($addResult -like "*Error*" -Or $addResult -like "*incorrect*") {
+	Write-Error "Error attempting to add binding: ${addResult}"
+} 
+else 
+{
+	Write-Output "Info: ${addResult}"
+}
