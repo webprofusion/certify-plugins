60a27b520a1b742282369a97869b93ef44810a83
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-11-02 11:44:23 +0800

    Move Nginx plugin into plugins repo

---
+++ src/ServerProviders/Nginx/Plugins.ServerProviders.Nginx/ServerProviderNginx.cs
@@ -0,0 +1,267 @@
+﻿using Certify.Models;
+using Certify.Models.Config;
+using Certify.Models.Providers;
+using Certify.Plugins.Server.Nginx;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Certify.Management.Servers
+{
+    public class ServerProviderNginx : ITargetWebServer
+    {
+        private ILog _log;
+        private NginxManager _nginxManager;
+
+        public static ProviderDefinition Definition
+        {
+            get
+            {
+                return new ProviderDefinition
+                {
+                    Id = "Plugin.ServerProviders.Nginx",
+                    Title = "NGINX",
+                    Description = "Queries local nginx config for site info",
+                    HelpUrl = "https://nginx.com"
+                };
+            }
+        }
+
+        public ServerProviderNginx()
+        {
+
+        }
+
+        public ServerProviderNginx(string configRoot, ILog log = null)
+        {
+            Init(log, configRoot);
+        }
+
+        public void Init(ILog log, string configRoot)
+        {
+            _log = log;
+
+            _nginxManager = new NginxManager(configRoot);
+        }
+
+        private NginxBindingDeploymentTarget _bindingDeploymentTarget = null;
+        public IBindingDeploymentTarget GetDeploymentTarget()
+        {
+            if (_bindingDeploymentTarget == null)
+            {
+                _bindingDeploymentTarget = new NginxBindingDeploymentTarget(this);
+            }
+            return _bindingDeploymentTarget;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public async Task<List<SiteInfo>> GetPrimarySites(bool ignoreStoppedSites)
+        {
+            return await _nginxManager.GetPrimarySites();
+        }
+
+        private string GetShellCommandOutput(string command)
+        {
+            // TODO: move this to a utility with common code from the Script.cs core deployment task
+            var output = "";
+
+            var shell = "cmd.exe";
+
+            // if running on *nix, identify if shell available : TODO: expand search method
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                shell = System.IO.File.Exists("/usr/bin/bash") ? "/usr/bin/bash" : "/bin/sh";
+                command = $"-c {command.Replace("\"", "\\\"")}";
+            }
+            else
+            {
+                command = $"{command.Replace("\"", "\\\"")}";
+            }
+
+
+            var startInfo = new ProcessStartInfo()
+            {
+                FileName = shell,
+                Arguments = command,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            var process = new Process() { StartInfo = startInfo };
+
+            process.OutputDataReceived += (obj, a) =>
+            {
+                if (a.Data != null)
+                {
+                    _log?.Information(a.Data);
+                    output += a.Data;
+                }
+            };
+
+            process.ErrorDataReceived += (obj, a) =>
+            {
+                if (!string.IsNullOrWhiteSpace(a.Data))
+                {
+                    _log?.Error($"Error: {a.Data}");
+                }
+            };
+
+            try
+            {
+                var timeoutMS = 1000 * 5;
+
+                process.Start();
+
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                process.WaitForExit(timeoutMS);
+            }
+            catch (Exception exp)
+            {
+                _log?.Error("Error Running Script: " + exp.ToString());
+            }
+
+            return output;
+
+        }
+        public async Task<Version> GetServerVersion()
+        {
+            // get nginx version e.g. "nginx version: nginx/1.18.0 (Ubuntu)"
+            var versionOutputString = await Task.Run<string>(() =>
+            {
+                return GetShellCommandOutput("nginx -v");
+            });
+
+            return GetServerVersion(versionOutputString);
+        }
+
+        public Version GetServerVersion(string versionOutputString)
+        {
+            var versionInfo = versionOutputString.Split("/".ToCharArray());
+
+            if (versionInfo.Length >= 2 && Version.TryParse(versionInfo[1].Split(' ')[0], out var versionResult))
+            {
+                return versionResult;
+            }
+            else
+            {
+                // could not parse
+                return new Version(0, 0, 0);
+            }
+        }
+
+        public Task<List<BindingInfo>> GetSiteBindingList(bool ignoreStoppedSites, string siteId = null)
+        {
+            return _nginxManager.GetBindings();
+        }
+
+        public async Task<SiteInfo> GetSiteById(string siteId)
+        {
+            var sites = await _nginxManager.GetPrimarySites();
+            return sites.FirstOrDefault(s => s.Id == siteId);
+        }
+
+        public Task<bool> IsAvailable()
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> IsSiteRunning(string id)
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task RemoveHttpsBinding(string siteId, string sni) => throw new NotImplementedException();
+
+        public Task<List<ActionStep>> RunConfigurationDiagnostics(string siteId)
+        {
+
+            return Task.FromResult(new List<ActionStep>());
+        }
+
+        public async Task<SiteInfo> CreateSite(List<string> hostnames, string phyPath, string protocol = "http", string ipAddress = "*", int? port = 80)
+        {
+            return _nginxManager.CreateSiteConfig(hostnames, phyPath, protocol, ipAddress, port);
+        }
+
+        public async Task<bool> DeleteSite(string primaryHostname)
+        {
+            return _nginxManager.DeleteSiteConfig(primaryHostname);
+        }
+
+        public Task<bool> SiteExists(string primaryHostname)
+        {
+            return _nginxManager.SiteExists(primaryHostname);
+        }
+
+        internal Task<ActionStep> AddOrUpdateSiteBinding(BindingInfo targetBinding, bool addNew)
+        {
+            // perform add/update of ssl binding config for the target site
+            return Task.FromResult(new ActionStep { Title = "No Deployment for nginx" });
+        }
+
+        public ServerTypeInfo GetServerTypeInfo()
+        {
+            return new ServerTypeInfo { ServerType = StandardServerTypes.Nginx, Title = "NGINX" };
+        }
+    }
+
+    public class NginxBindingTargetItem : IBindingDeploymentTargetItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class NginxBindingDeploymentTarget : IBindingDeploymentTarget
+    {
+        private readonly ServerProviderNginx _manager;
+
+        public NginxBindingDeploymentTarget(ServerProviderNginx manager)
+        {
+            _manager = manager;
+        }
+
+        public async Task<ActionStep> AddBinding(BindingInfo targetBinding) => await _manager.AddOrUpdateSiteBinding(targetBinding, true);
+
+        public async Task<ActionStep> UpdateBinding(BindingInfo targetBinding) => await _manager.AddOrUpdateSiteBinding(targetBinding, false);
+
+        public async Task<List<IBindingDeploymentTargetItem>> GetAllTargetItems()
+        {
+            var sites = await _manager.GetPrimarySites(true);
+
+            return sites.Select(s =>
+                (IBindingDeploymentTargetItem)new NginxBindingTargetItem
+                {
+                    Id = s.Id,
+                    Name = s.Name
+                }).ToList();
+        }
+
+        public async Task<List<BindingInfo>> GetBindings(string targetItemId) => await _manager.GetSiteBindingList(true, targetItemId);
+
+        public async Task<IBindingDeploymentTargetItem> GetTargetItem(string id)
+        {
+            var site = await _manager.GetSiteById(id);
+            if (site != null)
+            {
+                return new NginxBindingTargetItem { Id = site.Id, Name = site.Name };
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string GetTargetName() => "NGINX";
+    }
+}
