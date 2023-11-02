0e106e5f5d62d0075694bd1e4369a65deb41cb19
webprofusion-chrisc <christopher.cook@webprofusion.com>
2023-11-02 11:44:23 +0800

    Move Nginx plugin into plugins repo

---
+++ src/ServerProviders/Nginx/Plugins.ServerProviders.Nginx/Plugin.cs
@@ -0,0 +1,13 @@
+﻿using Certify.Models.Config;
+using Certify.Models.Plugins;
+using Certify.Models.Providers;
+using Certify.Plugins;
+
+namespace Plugin.ServerProviders.Core.Nginx
+{
+
+    public class Plugin : PluginProviderBase<ITargetWebServer, ProviderDefinition>, IServerProviderPlugin
+    {
+
+    }
+}
