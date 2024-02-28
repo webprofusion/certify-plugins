6b1a626056c9c47af8a1e5ae7bb1fdea49617c93
webprofusion-chrisc <christopher.cook@webprofusion.com>
2024-02-28 17:18:45 +0800

    Tasks: convert Set Private Key Permissions to C# from powershell, add config options

--- src/DeploymentTasks/Core/Providers/SetCertificateKeyPermissions.cs
+++ src/DeploymentTasks/Core/Providers/SetCertificateKeyPermissions.cs
@@ -2,8 +2,11 @@
 using Certify.Models;
 using Certify.Models.Config;
 using Plugin.DeploymentTasks.Shared;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace Certify.Providers.DeploymentTasks
@@ -13,24 +16,29 @@ namespace Certify.Providers.DeploymentTasks
         public static DeploymentProviderDefinition Definition { get; }
         public DeploymentProviderDefinition GetDefinition(DeploymentProviderDefinition currentDefinition = null) => (currentDefinition ?? Definition);
 
-        private const string SCRIPT_NAME = "SetCertificateKeyPermissions.ps1";
-
         static SetCertificateKeyPermissions()
         {
             Definition = new DeploymentProviderDefinition
             {
                 Id = "Certify.Providers.DeploymentTasks.SetCertificateKeyPermissions",
-                Title = "Set Certificate Key Permissions (RSA)",
-                DefaultTitle = "Set Certificate Key Permissions (RSA)",
+                Title = "Set Certificate Key Permissions)",
+                DefaultTitle = "Set Certificate Key Permissions",
                 IsExperimental = true,
                 UsageType = DeploymentProviderUsage.PostRequest,
                 SupportedContexts = DeploymentContextType.LocalAsService,
+                SupportsRemoteTarget = false,
                 Description = "Enable read access for the stored certificate private key for a specific account",
                 ProviderParameters = new List<ProviderParameter>
                 {
                       new ProviderParameter{ Key="account", Name="Account To Allow", IsRequired=true, IsCredential=false, Value="NT AUTHORITY\\LOCAL SERVICE"},
+                      new ProviderParameter{ Key="permission", Name="Permission To Grant", IsRequired=false, IsCredential=false, Value="read", OptionsList="read=Read;fullcontrol=Full Control;" },
                 }
             };
+        }
+
+        private List<ActionResult> PrepareErrorResult(string message)
+        {
+            return new List<ActionResult> { new ActionResult { IsSuccess = false, Message = message } };
         }
 
         public async Task<List<ActionResult>> Execute(DeploymentTaskExecutionParams execParams)
@@ -43,22 +51,67 @@ namespace Certify.Providers.DeploymentTasks
                 return validation;
             }
 
-            var script = Helpers.ReadStringResource(SCRIPT_NAME);
-
             var certRequest = execParams.Subject as CertificateRequestResult;
 
             execParams.Log?.Information("Executing command via PowerShell");
 
             var account = execParams.Settings.Parameters.FirstOrDefault(p => p.Key == "account")?.Value;
+            var permission = execParams.Settings.Parameters.FirstOrDefault(p => p.Key == "permission")?.Value ?? "read";
 
-            var parameters = new Dictionary<string, object>
+            var managedCert = ManagedCertificate.GetManagedCertificate(execParams.Subject);
+
+            if (string.IsNullOrEmpty(managedCert.CertificatePath) || !File.Exists(managedCert.CertificatePath))
             {
-                { "account", account }
-            };
+                return new List<ActionResult> { new ActionResult($"Certificate file not found: {managedCert.CertificatePath}", false) };
+            }
 
-            var scriptResult = await PowerShellManager.RunScript(execParams.Context.PowershellExecutionPolicy, certRequest, parameters: parameters, scriptContent: script, credentials: execParams.Credentials);
+            try
+            {
 
-            return new List<ActionResult> { scriptResult };
+                var settings = execParams.Settings;
+                var log = execParams.Log;
+
+                var pfxData = File.ReadAllBytes(managedCert.CertificatePath);
+
+                var certPwd = "";
+
+                if (!string.IsNullOrWhiteSpace(managedCert.CertificatePasswordCredentialId))
+                {
+                    var cred = await execParams.CredentialsManager.GetUnlockedCredentialsDictionary(managedCert.CertificatePasswordCredentialId);
+                    if (cred != null)
+                    {
+                        certPwd = cred["password"];
+                    }
+                    else
+                    {
+                        return PrepareErrorResult($"The credentials for this task could not be unlocked or were not accessible {managedCert.CertificatePasswordCredentialId}.");
+                    }
+                }
+
+                // get cert from store first otherwise it will be recreated on load with a new key file
+
+                var cert = CertificateManager.GetCertificateByThumbprint(managedCert.CertificateThumbprintHash);
+
+                if (cert == null)
+                {
+                    return PrepareErrorResult($"Certificate is not found in the machine certificate store. Private key permissions cannot be modified unless certificate has been stored.");
+                }
+
+                var certKeyPath = CertificateManager.GetCertificatePrivateKeyPath(cert);
+
+                if (CertificateManager.GrantUserAccessToCertificatePrivateKey(cert, account, fileSystemRights: permission, execParams.Log))
+                {
+                    return new List<ActionResult> { new ActionResult($"Certificate private key ({certKeyPath}) permissions updated for account {account}", true) };
+                }
+                else
+                {
+                    return PrepareErrorResult($"Failed to update certificate private key permissions updated for account {account}");
+                }
+            }
+            catch (Exception exp)
+            {
+                return PrepareErrorResult($"Failed to update certificate private key permissions updated for account {account}:: {exp}");
+            }
         }
 
         public async Task<List<ActionResult>> Validate(DeploymentTaskExecutionParams execParams)
