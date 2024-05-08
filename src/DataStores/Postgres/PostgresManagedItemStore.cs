cbcea4a2703b3d6447ee027e16ef9cf97cac9172
webprofusion-chrisc <christopher.cook@webprofusion.com>
2024-05-08 17:15:49 +0800

    Data store cleanup

--- src/DataStores/Postgres/PostgresManagedItemStore.cs
+++ src/DataStores/Postgres/PostgresManagedItemStore.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Certify.Models;
 using Certify.Models.Config;
 using Certify.Models.Providers;
+using Certify.Models.Reporting;
 using Certify.Providers;
 using Newtonsoft.Json;
 using Npgsql;
@@ -526,6 +527,14 @@ namespace Certify.Datastore.Postgres
             }
 
             return managedCertificate;
+        }
+
+        public async Task<Summary> GetSummary(ManagedCertificateFilter filter)
+        { 
+            var summary = new Summary();
+
+            summary.Total = (int)await CountAll(filter);
+            return summary;
         }
     }
 }
