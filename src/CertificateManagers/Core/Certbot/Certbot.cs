07cc0032af3704af344765a07b333a6707f94142
Christopher Cook <christopher.cook@webprofusion.com>
2024-09-10 10:58:07 +0800

    Cert managers: add provider definition method

--- src/CertificateManagers/Core/Certbot/Certbot.cs
+++ src/CertificateManagers/Core/Certbot/Certbot.cs
@@ -31,6 +31,11 @@ namespace Plugin.CertificateManagers
         {
 
         }
+        public ProviderDefinition GetProviderDefinition()
+        {
+            return Definition;
+        }
+
         public Task DeleteManagedCertificate(string id)
         {
             throw new NotImplementedException();
