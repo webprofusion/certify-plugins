3dafde122d4dc97b84fc2cdaaff778f22f7897fe
Christopher Cook <christopher.cook@webprofusion.com>
2025-03-18 12:32:24 +0800

    Cleanup

--- src/DataStores/SQLite/SQLiteStoreBase.cs
+++ src/DataStores/SQLite/SQLiteStoreBase.cs
@@ -98,7 +98,9 @@ namespace Certify.Datastore.SQLite
             return _initialised;
         }
 
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         protected virtual async Task<bool> UpgradeSettings()
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
             throw new NotImplementedException();
         }
