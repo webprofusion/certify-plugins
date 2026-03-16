c0e942c571a1d310815102c7c64a14d83b28dc0f
Christopher Cook <christopher.cook@webprofusion.com>
2026-03-16 16:09:57 +0800

    Tasks: Deploy to RDP Gateway - run PS as new process
    https://community.certifytheweb.com/t/error-with-deploying-to-rd-gateway/2730/3

--- src/DeploymentTasks/Core/Providers/RdpGateway.cs
+++ src/DeploymentTasks/Core/Providers/RdpGateway.cs
@@ -53,9 +53,9 @@ namespace Certify.Providers.DeploymentTasks
 
             var parameters = new Dictionary<string, object>();
 
-            var scriptResult = await PowerShellManager.RunScript(execParams.Context.PowershellExecutionPolicy, certRequest, parameters: parameters, scriptContent: script, credentials: execParams.Credentials, logonType: logonType);
+            var scriptResult = await PowerShellManager.RunScript(execParams.Context.PowershellExecutionPolicy, certRequest, parameters: parameters, scriptContent: script, credentials: execParams.Credentials, logonType: logonType, launchNewProcess: true);
 
-            return new List<ActionResult> { scriptResult };
+            return [scriptResult];
 
         }
 
