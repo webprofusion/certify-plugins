e0ed1eb05d3dbdbce712b58c67ccd7c0ef6bcd2b
Christopher Cook <christopher.cook@webprofusion.com>
2026-05-20 19:59:45 +0800

    RDP Gateway/Listener experimental switch to Cim

--- src/DeploymentTasks/Core/Providers/Assets/RDPGatewayServices.ps1
+++ src/DeploymentTasks/Core/Providers/Assets/RDPGatewayServices.ps1
@@ -1,15 +1,40 @@
-﻿# This is an example script and it will be overwritten when the next update is installed. 
-# To use this script copy it to another location and modify as required
-
-# Enable certificate for RDP Gateway
-# For more script info see https://docs.certifytheweb.com/docs/script-hooks.html
+﻿# Enable certificate for RDP Gateway
+# For more script info see https://docs.certifytheweb.com/docs/script-hooks
 
 param($result, [switch] $restartServices = $false)
 
-Import-Module RemoteDesktopServices
-
 # Apply certificate
-Set-Item -Path RDS:\GatewayServer\SSLCertificate\Thumbprint -Value $result.ManagedItem.CertificateThumbprintHash -ErrorAction Stop
+$thumbprint = $result.ManagedItem.CertificateThumbprintHash
+
+if ([string]::IsNullOrWhiteSpace($thumbprint)) {
+	throw "Certificate thumbprint was not supplied."
+}
+
+$thumbprint = ($thumbprint -replace '\s', '').ToUpperInvariant()
+$certHash = [byte[]]::new($thumbprint.Length / 2)
+
+for ($i = 0; $i -lt $certHash.Length; $i++) {
+	$certHash[$i] = [Convert]::ToByte($thumbprint.Substring($i * 2, 2), 16)
+}
+
+$gatewayNamespace = 'root/cimv2/TerminalServices'
+$gatewaySettings = Get-CimInstance -Namespace $gatewayNamespace -ClassName Win32_TSGatewayServerSettings -ErrorAction Stop | Select-Object -First 1
+
+if (-not $gatewaySettings) {
+	throw "RD Gateway server settings were not found."
+}
+
+$setCertificateResult = Invoke-CimMethod -InputObject $gatewaySettings -MethodName SetCertificate -Arguments @{ CertHash = $certHash } -ErrorAction Stop
+
+if ($setCertificateResult.ReturnValue -ne 0) {
+	throw "SetCertificate failed with return value $($setCertificateResult.ReturnValue)."
+}
+
+$configureResult = Invoke-CimMethod -InputObject $gatewaySettings -MethodName Configure -ErrorAction Stop
+
+if ($configureResult.ReturnValue -ne 0) {
+	throw "Configure failed with return value $($configureResult.ReturnValue)."
+}
 
 # Optionally restart TSGateway and related service
 
--- src/DeploymentTasks/Core/Providers/Assets/RDPListenerService.ps1
+++ src/DeploymentTasks/Core/Providers/Assets/RDPListenerService.ps1
@@ -1,14 +1,26 @@
-﻿# This is an example script and it will be overwritten when the next update is installed. 
-# To use this script copy it to another location and modify as required
-
-# Enable certificate for RDP Listener Service
-# For more script info see https://docs.certifytheweb.com/docs/script-hooks.html
+﻿# Enable certificate for RDP Listener Service
+# For more script info see https://docs.certifytheweb.com/docs/script-hooks
 
 # https://techcommunity.microsoft.com/t5/ask-the-performance-team/listener-certificate-configurations-in-windows-server-2012-2012/ba-p/375467
 
 param($result)
 
-# Apply certificate
-wmic /namespace:\\root\cimv2\TerminalServices PATH Win32_TSGeneralSetting Set SSLCertificateSHA1Hash="$($result.ManagedItem.CertificateThumbprintHash)"
+Import-Module CimCmdlets
 
-# TODO: need to set private key permissions?
+# Apply certificate
+$thumbprint = $result.ManagedItem.CertificateThumbprintHash
+
+if ([string]::IsNullOrWhiteSpace($thumbprint)) {
+    throw "Certificate thumbprint was not supplied."
+}
+
+$thumbprint = ($thumbprint -replace '\s', '').ToUpperInvariant()
+$rdpListenerNamespace = 'root/cimv2/TerminalServices'
+$rdpListener = Get-CimInstance -Namespace $rdpListenerNamespace -ClassName Win32_TSGeneralSetting -Filter "TerminalName='RDP-tcp'" -ErrorAction Stop
+
+if (-not $rdpListener) {
+    throw "RDP listener configuration was not found."
+}
+
+$null = $rdpListener | Set-CimInstance -Property @{ SSLCertificateSHA1Hash = $thumbprint } -ErrorAction Stop
+
